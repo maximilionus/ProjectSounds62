ac926ceb63ede0e048bd42df2a566c9f2a55c000
maximilionus <maksimka-up@yandex.ru>
2019-01-25 23:28:39 +0300

    movement -> footsteps

--- ps62_footsteps/_mov_asphalt_ext.hpp
+++ ps62_footsteps/_mov_asphalt_ext.hpp
@@ -142,147 +142,18 @@ class sprintErc_asphalt_ext_asphalt_ext_sprintErc_Char_SoundShader: baseCharacte
 	volume="(1 - bare) * 0.43";
 	frequency=1;
 };
-class walkErc_asphalt_ext_asphalt_ext_walkErc_Zmb_SoundShader: baseCharacter_SoundShader
+class walkErc_asphalt_ext_asphalt_ext_walkErc_Zmb_SoundShader: walkErc_asphalt_ext_asphalt_ext_walkErc_Char_SoundShader
 {
-	samples[]=
-	{
-		
-		{
-			"ProjectSounds62\ps62_data\sounds\characters\movement\boots\road\road_walk_1",
-			1
-		},
-		
-		{
-			"ProjectSounds62\ps62_data\sounds\characters\movement\boots\road\road_walk_2",
-			1
-		},
-		
-		{
-			"ProjectSounds62\ps62_data\sounds\characters\movement\boots\road\road_walk_3",
-			1
-		},
-		
-		{
-			"ProjectSounds62\ps62_data\sounds\characters\movement\boots\road\road_walk_4",
-			1
-		},
-		
-		{
-			"ProjectSounds62\ps62_data\sounds\characters\movement\boots\road\road_walk_5",
-			1
-		},
-		
-		{
-			"ProjectSounds62\ps62_data\sounds\characters\movement\boots\road\road_walk_6",
-			1
-		},
-		
-		{
-			"ProjectSounds62\ps62_data\sounds\characters\movement\boots\road\road_walk_7",
-			1
-		},
-		
-		{
-			"ProjectSounds62\ps62_data\sounds\characters\movement\boots\road\road_walk_8",
-			1
-		}
-	};
 	volume=0.2;
 	frequency=1;
 };
-class runErc_asphalt_ext_asphalt_ext_runErc_Zmb_SoundShader: baseCharacter_SoundShader
+class runErc_asphalt_ext_asphalt_ext_runErc_Zmb_SoundShader: runErc_asphalt_ext_asphalt_ext_runErc_Char_SoundShader
 {
-	samples[]=
-	{
-		
-		{
-			"ProjectSounds62\ps62_data\sounds\characters\movement\boots\road\road_run_1",
-			1
-		},
-		
-		{
-			"ProjectSounds62\ps62_data\sounds\characters\movement\boots\road\road_run_2",
-			1
-		},
-		
-		{
-			"ProjectSounds62\ps62_data\sounds\characters\movement\boots\road\road_run_3",
-			1
-		},
-		
-		{
-			"ProjectSounds62\ps62_data\sounds\characters\movement\boots\road\road_run_4",
-			1
-		},
-		
-		{
-			"ProjectSounds62\ps62_data\sounds\characters\movement\boots\road\road_run_5",
-			1
-		},
-		
-		{
-			"ProjectSounds62\ps62_data\sounds\characters\movement\boots\road\road_run_6",
-			1
-		},
-		
-		{
-			"ProjectSounds62\ps62_data\sounds\characters\movement\boots\road\road_run_7",
-			1
-		},
-		
-		{
-			"ProjectSounds62\ps62_data\sounds\characters\movement\boots\road\road_run_8",
-			1
-		}
-	};
 	volume=0.30000001;
 	frequency=1;
 };
-class sprintErc_asphalt_ext_asphalt_ext_sprintErc_Zmb_SoundShader: baseCharacter_SoundShader
+class sprintErc_asphalt_ext_asphalt_ext_sprintErc_Zmb_SoundShader: sprintErc_asphalt_ext_asphalt_ext_sprintErc_Char_SoundShader
 {
-	samples[]=
-	{
-		
-		{
-			"ProjectSounds62\ps62_data\sounds\characters\movement\boots\road\road_sprint_1",
-			1
-		},
-		
-		{
-			"ProjectSounds62\ps62_data\sounds\characters\movement\boots\road\road_sprint_2",
-			1
-		},
-		
-		{
-			"ProjectSounds62\ps62_data\sounds\characters\movement\boots\road\road_sprint_3",
-			1
-		},
-		
-		{
-			"ProjectSounds62\ps62_data\sounds\characters\movement\boots\road\road_sprint_4",
-			1
-		},
-		
-		{
-			"ProjectSounds62\ps62_data\sounds\characters\movement\boots\road\road_sprint_5",
-			1
-		},
-		
-		{
-			"ProjectSounds62\ps62_data\sounds\characters\movement\boots\road\road_sprint_6",
-			1
-		},
-		
-		{
-			"ProjectSounds62\ps62_data\sounds\characters\movement\boots\road\road_sprint_7",
-			1
-		},
-		
-		{
-			"ProjectSounds62\ps62_data\sounds\characters\movement\boots\road\road_sprint_8",
-			1
-		}
-	};
 	volume=0.43000001;
 	frequency=1;
 };
