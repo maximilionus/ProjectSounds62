c83632a68eccc41537a734890b993071016d70f4
maximilionus <maksimka-up@yandex.ru>
2019-01-25 22:39:26 +0300

    Initial commit

---
+++ ps62_data/config.cpp
@@ -0,0 +1,24 @@
+class CfgPatches
+{
+	class ps62_data
+	{
+		name = "Project Sounds62 - Data";
+		author = "MAXIMILI";
+		requiredVersion = 1.0; 
+		requiredAddons[] = {};
+		units[] = {};
+		weapons[] = {};
+	};
+};
+class CfgMods
+{
+	class ProjectSounds62
+	{
+		dir = "@ProjectSounds62";
+		name = "Project Sounds 62";
+		picture = "";
+		hidePicture = 1;
+		hideName = 0;
+		action = "";
+	};
+};
---
+++ ps62_footsteps/config.cpp
@@ -0,0 +1,18 @@
+class CfgPatches
+{
+	class ps62_footsteps
+	{
+		name = "Project Sounds62 - Footsteps Config";
+		author = "MAXIMILI";
+		requiredVersion = 1.0; 
+		requiredAddons[] = {"ps62_data"};
+		units[] = {};
+		weapons[] = {};
+	};
+};
+
+class CfgSoundShaders
+{
+	class baseCharacter_SoundShader;
+	#include "_mov_asphalt_ext.hpp"
+};
